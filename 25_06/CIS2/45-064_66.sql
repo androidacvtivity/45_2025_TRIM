a5402fb234e41cb48d58a2c5fe2a407a5eb3856b
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-09-10 16:05:21 +0300

    45-064

---
+++ 25_06/CIS2/45-064_66.sql
@@ -0,0 +1,29 @@
+SELECT
+DECODE(CC.NR_COLUMN, '1', 'COL1 ')||': '|| 
+  NVAL(SUM(CASE WHEN D.RIND IN ('5000') THEN DECODE(CC.NR_COLUMN, '1', D.COL1) END)) ||' < '||
+  NVAL(SUM(CASE WHEN D.RIND IN ('5100') THEN DECODE(CC.NR_COLUMN, '1', D.COL1) END))
+  AS REZULTAT
+
+FROM
+  VW_DATA_ALL_TEMP D,             
+       (                                                                        
+       SELECT '1' AS NR_COLUMN FROM DUAL                            
+       ) CC  
+WHERE
+  (D.PERIOADA IN (:PERIOADA)          ) AND
+  (D.CUIIO=:CUIIO                ) AND
+  (D.CUIIO_VERS=:CUIIO_VERS     OR :CUIIO_VERS = -1) AND
+  (D.FORM = :FORM               ) AND
+  (D.FORM_VERS=:FORM_VERS ) AND
+  (D.CAPITOL=:CAPITOL           OR :CAPITOL = -1) AND
+  (D.CAPITOL_VERS=:CAPITOL_VERS OR :CAPITOL_VERS = -1) AND
+  (D.ID_MD=:ID_MD               OR :ID_MD = -1) AND
+  
+  D.FORM IN (45)  AND
+  D.CAPITOL IN (399) AND
+  D.RIND IN ('5000','5100')
+GROUP BY
+  CC.NR_COLUMN
+HAVING
+  NVAL(SUM(CASE WHEN D.RIND IN ('5000') THEN DECODE(CC.NR_COLUMN, '1', D.COL1) END)) < 
+  NVAL(SUM(CASE WHEN D.RIND IN ('5100') THEN DECODE(CC.NR_COLUMN, '1', D.COL1) END))
